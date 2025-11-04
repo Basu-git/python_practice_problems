8888f0e9464475462866155954030e9a0b5c7f22
Basu <basavarajumachagi99@gmail.com>
2025-11-04 20:18:43 +0530

    initial commit

---
+++ Loop_problems/Print the sum of first 10 natural numbers.py
@@ -0,0 +1,14 @@
+#Print the sum of first 10 or n natural numbers.
+n=int(input("Enter the number : "))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print(sum)
+
+
+
+#"it can be written using while loop also 
+# sum=0
+# while n<11:
+#       sum+=i
+#print(sum)"
