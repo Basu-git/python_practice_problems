8f8900e28e23f1658ae05cfccc7bbaea5af2bb28
Basu <basavarajumachagi99@gmail.com>
2025-11-11 19:43:40 +0530

    initial commit

---
+++ Practiced_codes/lowercase_converter.py
@@ -0,0 +1,7 @@
+#### 26. Lowercase Converter (Easy)
+#**Description:** Convert input string to lowercase.
+#**Example Input:** HeLLo
+#**Expected Output (blueprint):** hello
+n=input("Enter a word: ")
+print(n)
+print(n.lower())
