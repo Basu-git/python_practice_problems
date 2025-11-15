66fb31c1d9a5450e9b6215b73507433dee730319
Basu <basavarajumachagi99@gmail.com>
2025-11-16 01:09:48 +0530

    intial commit

--- Practiced_codes/List_rotation.py
+++ Practiced_codes/List_rotation.py
@@ -1,10 +1,2 @@
-### 45. Rotate List (Medium)
-#**Description:** Rotate list right by k positions.
-#**Example Input:** [1,2,3,4,5], k=2
-#**Expected Output (blueprint):** [4,5,1,2,3]
-###
-def rotate_right(nums, k):
-    k = k % len(nums)        # handle k larger than length
-    return nums[-k:] + nums[:-k]
-
-print(rotate_right([1,2,3,4,5], 2))   # Output: [4,5,1,2,3]
+n=[1,2,3,4,5]
+print(n[-2:]+n[:-2])
