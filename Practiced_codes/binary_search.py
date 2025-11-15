ef8629afd31af4deb78ae51db948703fbb981fdd
Basu <basavarajumachagi99@gmail.com>
2025-11-16 01:13:53 +0530

    initial commit

---
+++ Practiced_codes/binary_search.py
@@ -0,0 +1,6 @@
+#### 46. Binary Search (Medium)
+#**Description:** Given sorted list and key, return index or -1.
+#**Example Input:** [1,3,5,7], key=5
+#**Expected Output (blueprint):** 2
+n=[1,3,5,7]
+print(n.index(5))
